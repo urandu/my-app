1b6eab2f2ee1a7dbdff09abe9a3df0b3558fec7d
bildad namawa <bildadnamawa@gmail.com>
2019-10-21 08:13:43 +0300

    auto-commit on change (2019-10-21 08:13:43) by piladi-bot

--- src/counter.jsx
+++ src/counter.jsx
@@ -1,11 +1,16 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 
 export default class Counter extends Component {
     state = {value: 1};
 
     render() {
         return (
-            <div>{this.state.value}</div>
+            <Fragment>
+                <div>{this.state.value}</div>
+                <button onClick={() => { console.log('clicked'); }}>Increment</button>
+
+            </Fragment>
+
             
         );
     }
