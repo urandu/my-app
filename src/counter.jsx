9bf773f44f0717a8b86c308178ad9553da25543b
bildad namawa <bildadnamawa@gmail.com>
2019-10-21 22:40:34 +0300

    auto-commit on change (2019-10-21 22:40:34) by piladi-bot

--- src/counter.jsx
+++ src/counter.jsx
@@ -6,6 +6,7 @@ export default class Counter extends Component {
     render() {
         return (
             <Fragment>
+                <h1>{this.props.name}</h1>
                 <div>{this.state.value}</div>
                 <button onClick={() => { this.setState({value:this.state.value + 1}) }}>Increment</button>
 
