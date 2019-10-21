93dffe72b213039065f83b8deda69bb514ebf0b9
bildad namawa <bildadnamawa@gmail.com>
2019-10-22 01:26:16 +0300

    auto-commit on change (2019-10-22 01:26:16) by piladi-bot

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Counter from './counter';
 import Table from './components/Table';
+import App from "./App";
 
 
 
 
-ReactDOM.render(< Table />, document.getElementById('root'));
+ReactDOM.render(< App />, document.getElementById('root'));
