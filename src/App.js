13b754125702b12456b7a2352fd08183e738c21e
bildad namawa <bildadnamawa@gmail.com>
2019-10-22 01:27:33 +0300

    auto-commit on change (2019-10-22 01:27:33) by piladi-bot

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 import Table from "./components/Table";
 
 class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            droplets: []
+        }
+    }
     render() {
         return (
             <div className="App">
